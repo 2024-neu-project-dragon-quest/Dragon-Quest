1e716ce21d64ea3c4922abf5cf42cd2298d049e6
FIAS-School <fiasprjt@gmail.com>
2024-06-06 20:24:31 +0200

    i spent 2 hours working on one stupid little thing, this is such a waste of time, and i am STILL NOT DONE WITH IT RAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHH

--- DragonQuest/Misc/RandomValue.cs
+++ DragonQuest/Misc/RandomValue.cs
@@ -7,33 +7,110 @@ namespace DragonQuest.Misc {
 
     public class RandomValue {
         
-        private double[,] ranges;
+        /*
+        
+            THERE IS A PROBLEM WITH THIS:
+        
+            It's not all uniform probability.
+
+            1-20;69
+
+            69 has more probability than any of the numbers between 1-20
+
+            UPDATE:
+
+            okay i fixed it lol
+
+        */
+
+        private double[][] ranges;
+        private double[][] urm;
 
         public RandomValue(string range) {
 
             string[] ranges = range.Split(";");
-            this.ranges = new double[ranges.Length, 2];
+            this.ranges = new double[ranges.Length][];
 
             for (int i = 0; i < ranges.Length; i++) {
 
                 string[] sRange = ranges[i].Split("-");
-                this.ranges[i, 0] = double.Parse(sRange[0]);
-                this.ranges[i, 1] = double.Parse(sRange[1]);
+                if (sRange.Length < 2) {
+
+                    this.ranges[i] = new double[] {double.Parse(ranges[i]), double.Parse(ranges[i])};
+                    continue;
+
+                }
+
+                this.ranges[i] = new double[] {double.Parse(sRange[0]), double.Parse(sRange[1])};
 
             }
 
+            this.urm = GetUniformRangeMap();
+
         }
 
-        private double RangeSum() {
-            
+        private int[] GetRangeDifferenceMap() {
+
+            int[] rangeDiffMap = new int[this.ranges.Length];
+            for (int i = 0; i < this.ranges.Length; i++) {
+
+                int diff = (int) Math.Round(this.ranges[i][1] - this.ranges[i][0]);    
+                rangeDiffMap[i] = diff == 0 ? 1 : diff;
+
+            }
+
+            return rangeDiffMap;
+
+        }
+
+        private int GetRangeSum() {
+
+            int sum = 0;
+            foreach (int rd in GetRangeDifferenceMap()) sum += rd;
+
+            return sum;
+
+        }
+
+        private double[][] GetUniformRangeMap() {
+
             /* calculate the range sum here */
-            return 69;
+            double[][] urm = new double[GetRangeSum()][];
+            int[] rdm = GetRangeDifferenceMap();
+
+            int urmIndex = 0;
+            for (int i = 0; i < rdm.Length; i++)
+                for (int j = 0; j < rdm[i]; j++) {
+
+                    urm[urmIndex] = new double[] {this.ranges[i][0], this.ranges[i][1]};
+                    urmIndex++;
+
+                }
+
+            return urm;
+
+        }
+
+        private double[] GetRandomRange() {
+
+            int randomRangeIndex = (int) (this.urm.Length * new Random().NextDouble());
+            double rangeMin = this.urm[randomRangeIndex][0],
+                   rangeMax = this.urm[randomRangeIndex][1];
+
+            return new double[] {rangeMin, rangeMax}; 
         
         }
 
-        public double GetDouble() { return 69; }
+        public double GetDouble() {
 
-        public int GetInt() { return 69; }
+            double[] randomRange = GetRandomRange();
+            double rangeDiff = randomRange[1] - randomRange[0];
+
+            return rangeDiff * new Random().NextDouble() + randomRange[0];
+
+        }
+
+        public int GetInt() { return (int) Math.Round(GetDouble()); }
 
     }
 
--- DragonQuest/Program.cs
+++ DragonQuest/Program.cs
@@ -31,3 +31,8 @@ containers, mobs and items are ENTIDIESSSSS
 note for myself: there needs to be a relation between items and mobs, as all items that are on the map and can be picked up (and also have an on-map texture) are treated as entities with no brain
 
 */
+
+Console.WriteLine("skibidi skibidi skibidi skibidi skibidi\n\n\n\nAMBATUKAMMMM\n\n\n\n");
+
+RandomValue rv = new RandomValue("2-6;69;45-50");
+for (int i = 0; i < 100; i++) Console.WriteLine(rv.GetInt());
